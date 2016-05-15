0ba0385eba111cad5f9037fa4a667a29e8f408cb
Jm Casler <jm@casler.org>
2016-05-15 03:33:59 -0700

    There's a bug in the ArduinoOTA library where it overwrites the mdns responder. Removing support for ArduinoOTA for the time being.

--- source/v0.0.2/eflow/eflow.ino
+++ source/v0.0.2/eflow/eflow.ino
@@ -24,8 +24,6 @@
 
 #include <ESP8266HTTPUpdateServer.h>
 
-#include <WiFiUdp.h>
-#include <ArduinoOTA.h>
 
 
 // Configuration Start
@@ -329,24 +327,6 @@ void setup() {
     
   }
 
-  ArduinoOTA.onStart([]() {
-    Serial.println("Start");
-  });
-  ArduinoOTA.onEnd([]() {
-    Serial.println("\nEnd");
-  });
-  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
-    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
-  });
-  ArduinoOTA.onError([](ota_error_t error) {
-    Serial.printf("Error[%u]: ", error);
-    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
-    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
-    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
-    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
-    else if (error == OTA_END_ERROR) Serial.println("End Failed");
-  });
-  ArduinoOTA.begin();
 
 
 }
@@ -357,8 +337,6 @@ void handleRoot2() {
 
 void loop() {
 
-  ArduinoOTA.handle();
-  
 
   updateSensors();
 
